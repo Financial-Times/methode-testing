3336eef86f0e37764e0882184d64c2c978e07a97
Simon.Gibbs <simon.gibbs@ft.com>
2013-11-18 16:22:12 +0000

    Reference articles helper object
    Merge branch 'master' of http://git.svc.ft.com:9080/git/content-replatform-sandbox/methode-synchronizer
    
    Conflicts:
    	methode-bridge-acceptance/src/test/java/com/ft/methodebridge/acceptance/MethodeArticle.java

--- src/main/java/com/ft/methodetesting/MethodeArticle.java
+++ src/main/java/com/ft/methodetesting/MethodeArticle.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import com.ft.methodeapi.model.EomFile;
 import com.google.common.base.Charsets;
 import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
 import com.google.common.io.Resources;
 
 public class MethodeArticle {
@@ -12,9 +13,6 @@ public class MethodeArticle {
 	public static final String HEADLINE_FROM_TEST_FILE = "Eurozone collapses ...! Kaboom.";
 	public static final String MARK_DELETED_TRUE = "<DIFTcomMarkDeleted>True</DIFTcomMarkDeleted>";
 	public static final String MARK_DELETED_FALSE = "<DIFTcomMarkDeleted>False</DIFTcomMarkDeleted>";
-	
-	private static final String exampleArticleXmlTemplate = readFromFile("examplePublishedArticleData.txt");
-	private static final String exampleAttributesXml = readFromFile("examplePublishedArticleAttributes.txt");
 
 	private String articleXml;
 	private String attributesXml;
@@ -46,42 +44,53 @@ public class MethodeArticle {
                 .add("attributesXml", attributesXml)
                 .toString();
     }
+
+    public static Builder builder(String articleXml, String attributesXml) {
+        Builder builder = new Builder();
+        builder.articleXml = articleXml;
+        builder.attributesXml = attributesXml;
+        return builder;
+    }
 	
 	public static class Builder {
 
-		String articleXml = exampleArticleXmlTemplate;
-		String attributesXml = exampleAttributesXml;
+		private String articleXml;
+		private String attributesXml;
 
 		public Builder withHeadline(String expectedPublishedArticleHeadline) {
 	        attributesXml = attributesXml.replace(HEADLINE_FROM_TEST_FILE,expectedPublishedArticleHeadline);
 	        articleXml = articleXml.replace(HEADLINE_FROM_TEST_FILE, expectedPublishedArticleHeadline);
 			return this;
 		}
+
+        public Builder published() {
+            Preconditions.checkArgument(!this.attributesXml.contains(MARK_DELETED_TRUE),"already deleted");
+            return this;
+        }
+
+        public Builder deleted() {
+            attributesXml = attributesXml.replace(MARK_DELETED_FALSE, MARK_DELETED_TRUE);
+            return this;
+        }
 		
 		public MethodeArticle buildPublishedArticle() {
-			return new MethodeArticle(articleXml, attributesXml);
+			return published().article();
 		}
 
 		public MethodeArticle buildDeletedArticle() {
-			attributesXml = attributesXml.replace(MARK_DELETED_FALSE, MARK_DELETED_TRUE);
-			return new MethodeArticle(articleXml, attributesXml);
+            return deleted().article();
 		}
+
+        public MethodeArticle article() {
+            return build();
+        }
+
+        public MethodeArticle build() {
+            return new MethodeArticle(articleXml, attributesXml);
+        }
 
 	}
 	
-	private static String readFromFile(String resourceName) {
-		String bodyFromFile = "";
-		try {
-			bodyFromFile = Resources.toString(Resources.getResource(resourceName), Charsets.UTF_8);
-		
-			// because what we get back from the API uses UNIX line encodings, but when working locally on Windows, the expected file will have \r\n
-			if (System.getProperty("line.separator").equals("\r\n")) {
-				bodyFromFile = bodyFromFile.replace("\r", "");
-			}
-		} catch (IOException e) {
-			throw new RuntimeException("Unexpected error reading from content in JAR file",e);
-		}
-		return bodyFromFile;
-	}
+
 
 }
---
+++ src/main/java/com/ft/methodetesting/ReferenceArticles.java
@@ -0,0 +1,46 @@
+package com.ft.methodetesting;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
+
+import java.io.IOException;
+
+/**
+ * ReferenceArticles
+ *
+ * @author Simon.Gibbs
+ */
+public class ReferenceArticles {
+
+    private static final String exampleArticleXmlTemplate = readFromFile("ArticleWithEverything.xml");
+    private static final String exampleAttributesXml = readFromFile("ArticleWithEverythingAttributes.xml");
+
+    public static MethodeArticle.Builder publishedKitchenSinkArticle() {
+        return MethodeArticle.builder(exampleArticleXmlTemplate, exampleAttributesXml).published();
+    }
+
+    public static MethodeArticle.Builder unpublishedKitchenSinkArticle() {
+        return MethodeArticle.builder(exampleArticleXmlTemplate,exampleAttributesXml).deleted();
+    }
+
+    public static String expectedKichenSinkTransformedXml() {
+        return readFromFile("ArticleWithEverythingAttributes.xml");
+    }
+
+
+    private static String readFromFile(String resourceName) {
+        String bodyFromFile = "";
+        try {
+            bodyFromFile = Resources.toString(Resources.getResource(resourceName), Charsets.UTF_8);
+
+            // because what we get back from the API uses UNIX line encodings, but when working locally on Windows, the expected file will have \r\n
+            if (System.getProperty("line.separator").equals("\r\n")) {
+                bodyFromFile = bodyFromFile.replace("\r", "");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Unexpected error reading from content in JAR file",e);
+        }
+        return bodyFromFile;
+    }
+
+}
