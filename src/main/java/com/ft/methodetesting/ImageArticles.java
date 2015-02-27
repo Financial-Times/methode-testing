4262c151d384c2c3a4dc7c847c56b2b0f8df0c76
Dragos Serghie <dragos.serghie@ft.com>
2015-02-27 14:33:27 +0200

    UPP-265: Added capabilities to set article main images display size

--- src/main/java/com/ft/methodetesting/ImageArticles.java
+++ src/main/java/com/ft/methodetesting/ImageArticles.java
@@ -21,6 +21,7 @@ public class ImageArticles {
     private static final String STORY_TEMPLATE = "/SysConfig/Templates/FT/Base-Story.xml";
     private static final Charset UTF_8 = Charset.forName("UTF-8");
     public static final String IMAGE_SET_ONTOLOGY = "http://www.ft.com/ontology/content/ImageSet";
+    public static final String DEFAULT_MAIN_IMAGE_SIZE = "Primary size";
     private static final String simpleArticleXmlTemplate = FileUtil.loadFile("article/simple_article_value.xml");
     private static final String articleWithMainImageXmlTemplate = FileUtil.loadFile("article/article_value_with_main_image.xml");
     private static final String articleWithInlineImageXmlTemplate = FileUtil.loadFile("article/article_value_with_inline_image.xml");
@@ -34,43 +35,43 @@ public class ImageArticles {
                 .withUuid(articleUuid)
                 .withType(STORY_TYPE)
                 .withValue(simpleArticleXmlTemplate.getBytes(UTF_8))
-                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, SOURCE_CODE))
+                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, DEFAULT_MAIN_IMAGE_SIZE, SOURCE_CODE))
                 .withSystemAttributes(String.format(articleSystemAttributesXml, CHANEL, STORY_TEMPLATE))
                 .withWorkflowStatus(workflowStatus)
                 .build();
     }
 
-    public static EomFile buildArticleWithMainImage(String articleUuid, String mainImageUuid, String markedDeleted, String workflowStatus) throws IOException {
+    public static EomFile buildArticleWithMainImage(String articleUuid, String mainImageUuid, String markedDeleted, String mainImageSize, String workflowStatus) throws IOException {
 
         return new EomFile.Builder()
                 .withUuid(articleUuid)
                 .withType(STORY_TYPE)
                 .withValue(String.format(articleWithMainImageXmlTemplate, mainImageUuid).getBytes(UTF_8))
-                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, SOURCE_CODE))
+                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, mainImageSize, SOURCE_CODE))
                 .withSystemAttributes(String.format(articleSystemAttributesXml, CHANEL, STORY_TEMPLATE))
                 .withWorkflowStatus(workflowStatus)
                 .build();
     }
 
-    public static EomFile buildArticleWithInlineImage(String articleUuid, String inlineImageUuid, String markedDeleted, String workflowStatus) throws IOException {
+    public static EomFile buildArticleWithInlineImage(String articleUuid, String inlineImageUuid, String markedDeleted, String mainImageSize, String workflowStatus) throws IOException {
 
         return new EomFile.Builder()
                 .withUuid(articleUuid)
                 .withType(STORY_TYPE)
                 .withValue(String.format(articleWithInlineImageXmlTemplate, inlineImageUuid).getBytes(UTF_8))
-                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, SOURCE_CODE))
+                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, mainImageSize, SOURCE_CODE))
                 .withSystemAttributes(String.format(articleSystemAttributesXml, CHANEL, STORY_TEMPLATE))
                 .withWorkflowStatus(workflowStatus)
                 .build();
     }
 
-    public static EomFile buildArticleWithMainImageAndInlineImage(String articleUuid, String mainImageUuid, String inlineImageUuid, String markedDeleted, String workflowStatus) throws IOException {
+    public static EomFile buildArticleWithMainImageAndInlineImage(String articleUuid, String mainImageUuid, String inlineImageUuid, String markedDeleted, String mainImageSize, String workflowStatus) throws IOException {
 
         return new EomFile.Builder()
                 .withUuid(articleUuid)
                 .withType(STORY_TYPE)
                 .withValue(String.format(articleWithMainAndInlineImageXmlTemplate, mainImageUuid, inlineImageUuid).getBytes(UTF_8))
-                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, SOURCE_CODE))
+                .withAttributes(String.format(articleAttributesXml, methodeFormatDate(), markedDeleted, mainImageSize, SOURCE_CODE))
                 .withSystemAttributes(String.format(articleSystemAttributesXml, CHANEL, STORY_TEMPLATE))
                 .withWorkflowStatus(workflowStatus)
                 .build();
